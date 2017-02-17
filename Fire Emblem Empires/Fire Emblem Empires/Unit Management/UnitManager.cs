217bd7850ed363149c1e4debaaa74fb8233d038e
Trent Castro <KeimaAyasaki@gmail.com>
2017-02-16 18:44:03 -0700

    commiting an updated unit

--- Fire Emblem Empires/Fire Emblem Empires/Unit Management/UnitManager.cs
+++ Fire Emblem Empires/Fire Emblem Empires/Unit Management/UnitManager.cs
@@ -6,12 +6,7 @@ using System.Threading.Tasks;
 
 namespace Fire_Emblem_Empires.Unit_Management
 {
-    public class Mercenary : Unit
+    class UnitManager
     {
-
-       public Mercenary()
-        {
-
-        }
     }
 }
