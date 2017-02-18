a60dff85047e40a85c118e2981a25f3278bfe8ec
Daniel Nunez <dmnunez3@yahoo.com>
2017-02-17 18:24:35 -0700

    Created general methods for Board such as a constructor and setting the values of a specific space.

---
+++ Fire Emblem Empires/Fire Emblem Empires/Board.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fire_Emblem_Empires
+{
+
+    class Tile
+    {
+        public Tile(TileEnumeration terrain, Unit unit)
+        {
+            terrainType = terrain;
+            occupiedBy = unit;
+        }
+        public TileEnumeration terrainType;
+        public Unit occupiedBy;
+    }
+    class Board
+    {
+        private Tile[,] spaces;
+
+        public Board(int numRows, int numColumns)
+        {
+            spaces = new Tile[numRows, numColumns];
+            foreach(Tile space in spaces)
+            {
+                space.terrainType = TileEnumeration.PLAIN;
+            }
+        }
+
+        public void SetSpace(int row, int column, Tile desiredTile)
+        {
+            spaces[row, column] = desiredTile;
+        }
+    }
+}
