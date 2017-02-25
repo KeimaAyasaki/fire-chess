c9103788d4c668f2b9883914240a2537ea92d071
ZFlem96 <zackzacharyfleming@gmail.com>
2017-02-24 18:09:09 -0700

    items tested

--- Fire Emblem Empires/Fire Emblem Empires/Items/ItemManager.cs
+++ Fire Emblem Empires/Fire Emblem Empires/Items/ItemManager.cs
@@ -30,27 +30,7 @@ namespace Fire_Emblem_Empires.Items
 
         public static void main(String[] args)
         {
-            IronSword sword = new IronSword();
-            System.Console.WriteLine("Sword has"+sword.getMight()+" might.");
-            IronLance lance = new IronLance();
-            System.Console.WriteLine("Lance has" + lance.getMight() + " might.");
-            IronAxe axe = new IronAxe();
-            System.Console.WriteLine("Axe has" + axe.getMight() + " might.");
-            Fire flames = new Fire();
-            System.Console.WriteLine("Flames has" + flames.getMight() + " might.");
-            Staff staff = new Staff();
-            System.Console.WriteLine("Flames has" + staff.getMight() + " might.");
-            Vulnerary vul = new Vulnerary();
-            System.Console.WriteLine("Vul has "+vul.getMight()+" might and currently"+vul.getDurability()+" durability.");
-            ItemManager mg = new ItemManager();
-            mg.useVulnenary(vul);
-            System.Console.WriteLine("Vul's durability is now at "+vul.getDurability());
-            mg.useVulnenary(vul);
-            System.Console.WriteLine("Vul's durability is now at " + vul.getDurability());
-            mg.useVulnenary(vul);
-            System.Console.WriteLine("Vul's durability is now at " + vul.getDurability());
-            mg.useVulnenary(vul);
-            System.Console.WriteLine("Vul's durability is now at " + vul.getDurability());
+            
         }
     }
 }
