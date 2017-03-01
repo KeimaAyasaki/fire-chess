16ac9a9b7756abde53dae4c511627962721e08e4
ZFlem96 <zackzacharyfleming@gmail.com>
2017-02-28 18:28:31 -0700

    Worked on images for the menu screen

--- Fire Emblem Empires/Fire Emblem Empires/Item Management/ItemManager.cs
+++ Fire Emblem Empires/Fire Emblem Empires/Item Management/ItemManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//Worked on images for the menu screen
 namespace Fire_Emblem_Empires.Item_Management
 {
     public class ItemManager
