d8d3ae7199338d1d692663fedf5c304a1982c213
ZFlem96 <zackzacharyfleming@gmail.com>
2017-03-03 17:10:47 -0700

    removed comment

--- Fire Emblem Empires/Fire Emblem Empires/Item Management/ItemManager.cs
+++ Fire Emblem Empires/Fire Emblem Empires/Item Management/ItemManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//Worked on images for the menu screen
 namespace Fire_Emblem_Empires.Item_Management
 {
     public class ItemManager
