266d9010a9d55419c922e99e6ef486637db927f9
Trent Castro <KeimaAyasaki@gmail.com>
2017-03-06 17:39:31 -0700

    Fixed battle manager unit tests

---
+++ Fire Emblem Empires/Fire Emblem Empires Tests/Battle Unit Tests.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fire_Emblem_Empires;
+using Fire_Emblem_Empires.Unit_Creation;
+using Fire_Emblem_Empires.Battle_Management;
+
+namespace Fire_Emblem_Empires_Tests
+{
+    [TestClass]
+    public class EstebanTest
+    {
+        BattleManager bm = new BattleManager();
+        //Unit(Color, HPMAX, HPCurrent, ATK, SPD, DEF, RES)
+        Unit fb1 = new Fighter(Team.BLUE, 27, 27, 10, 5, 4, 0, true);
+        Unit sb1 = new Soldier(Team.BLUE, 24, 24, 7, 4, 8, 1, true);
+        Unit mb1 = new Mercenary(Team.BLUE, 21, 15, 6, 8, 4, 1, true);
+        Unit hb1 = new Healer(Team.BLUE, 18, 18, 4, 4, 2, 5, true);
+
+        Unit fr1 = new Fighter(Team.RED, 27, 27, 10, 5, 4, 0, true);
+        Unit sr1 = new Soldier(Team.RED, 24, 24, 7, 4, 8, 1, true);
+        Unit mr1 = new Mercenary(Team.RED, 21, 15, 6, 8, 4, 1, true);
+        Unit hr1 = new Healer(Team.RED, 18, 18, 4, 4, 2, 5, true);
+
+        Unit bHighStatFighter   = new Fighter(Team.BLUE, 90, 90, 90, 90, 90, 90, true);
+        Unit bLowStatFighter    = new Fighter(Team.BLUE, 5, 5, 5, 5, 5, 5, true);
+        Unit bHealerOne         = new Healer(Team.BLUE, 50, 50, 50, 5, 5, 5, true);
+        Unit bInjuredSoldierOne = new Soldier(Team.BLUE, 50, 40, 5, 5, 5, 5, true);
+        
+
+        [TestMethod]
+        public void TestRedUnitDamageCalculation()
+        {
+            var expectedDamageRed = 2;
+            byte actualDamageRed = 0;
+            bm.calculateDamage(mb1, mr1, out actualDamageRed);
+            Assert.AreEqual(expectedDamageRed, actualDamageRed);
+        }
+
+        [TestMethod]
+        public void TestBlueUnitDamageCalculation()
+        {
+            var expectedDamageBlue = 2;
+            byte actualDamageBlue = 0;
+            bm.calculateDamage(mr1, mb1, out actualDamageBlue);
+            Assert.AreEqual(expectedDamageBlue, actualDamageBlue);
+        }
+
+        [TestMethod]
+        public void TestBlueUnitHealedAmountCalculation()
+        {
+            var expectedHealedAmountBlue = 6;
+            byte actualHealedAmountBlue = 6;
+            bm.calculateHealing(hb1, mb1, out actualHealedAmountBlue);
+            Assert.AreEqual(expectedHealedAmountBlue, actualHealedAmountBlue);
+        }
+
+        [TestMethod]
+        public void TestRedUnitHealedAmountCalculation()
+        {
+            var expectedHealedAmountRed = 6;
+            byte actualHealedAmountRed = 6;
+            bm.calculateHealing(hr1, mr1, out actualHealedAmountRed);
+            Assert.AreEqual(expectedHealedAmountRed, actualHealedAmountRed);
+        }
+
+        [TestMethod]
+        public void UnitsCannotAttackAllies()
+        {
+            byte expectedByteResult = 0;
+            byte actualByteResult   = 255;
+
+            bool expectedBoolResult = false;
+            bool actualBoolResult   = bm.calculateDamage(bHighStatFighter, bLowStatFighter, out actualByteResult);
+            Assert.AreEqual(expectedByteResult, actualByteResult);
+            Assert.AreEqual(expectedBoolResult, actualBoolResult);
+        }
+
+        [TestMethod]
+        public void UnitsWhoCannotHealCannotHeal()
+        {
+            byte expectedByteResult = 0;
+            byte actualByteResult = 255;
+
+            bool expectedBoolResult = false;
+            bool actualBoolResult = bm.calculateHealing(bHighStatFighter, bLowStatFighter, out actualByteResult);
+            Assert.AreEqual(expectedByteResult, actualByteResult);
+            Assert.AreEqual(expectedBoolResult, actualBoolResult);
+        }
+
+        [TestMethod]
+        public void UnitsCannotOverheal()
+        {
+            byte expectedByteResult = 10;
+            byte actualByteResult = 255;
+
+            bool expectedBoolResult = true;
+            bool actualBoolResult = bm.calculateHealing(bHealerOne, bInjuredSoldierOne, out actualByteResult);
+            Assert.AreEqual(expectedByteResult, actualByteResult);
+            Assert.AreEqual(expectedBoolResult, actualBoolResult);
+        }
+    }
+}
