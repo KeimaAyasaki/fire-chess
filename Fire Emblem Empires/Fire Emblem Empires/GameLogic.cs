102db8a8eb0e7790693ef44f6272bd94eb87481f
ealba2615 <ealba2615@gmail.com>
2017-03-09 19:06:50 -0700

    More Logic

--- Fire Emblem Empires/Fire Emblem Empires/GameLogic.cs
+++ Fire Emblem Empires/Fire Emblem Empires/GameLogic.cs
@@ -1,4 +1,6 @@
-﻿using Fire_Emblem_Empires.Unit_Creation;
+﻿using Fire_Emblem_Empires.Board_Creation;
+using Fire_Emblem_Empires.Unit_Creation;
+using Fire_Emblem_Empires.Battle_Management;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +14,14 @@ namespace Fire_Emblem_Empires
         public static Unit[] p1_roster = new Unit[] { new Fighter(Team.BLUE), new Healer(Team.BLUE), new Mage(Team.BLUE), new Mercenary(Team.BLUE), new Soldier(Team.BLUE) };
         public static Unit[] p2_roster = new Unit[] { new Fighter(Team.RED), new Healer(Team.RED), new Mage(Team.RED), new Mercenary(Team.RED), new Soldier(Team.RED) };
         static bool p1Turn = true;
+        public Board m_board { get; private set; }
+        public BattleManager m_battleManager { get; private set; }
+
+        public GameLogic(ref Board board)
+        {
+            m_board = board;
+            m_battleManager = new BattleManager();
+        }
 
         Player p1 = new Player(p1_roster, 5, Team.BLUE);
         Player p2 = new Player(p2_roster, 5, Team.RED);
@@ -57,7 +67,86 @@ namespace Fire_Emblem_Empires
 
         private void takeTurn(Player p)
         {
-            throw new NotImplementedException();
+            p.canNowMoveAllUnits();
+            while (p.CanMoveUnits())
+            {
+                Tile currTile = selectTile();
+                // while boolean over here for unit opreation
+                Tile destTile = selectTile();
+                //Checks if the distance the player wishes to move is greater than the movement the unit has
+                if(m_board.CalculateDistance(currTile, destTile) > currTile.m_unit.m_MovementRange)
+                {
+                    continue;
+                }
+                if (BothTilesHaveAUnit(currTile, destTile))
+                {
+                    //if units can interact
+                    UnitsInteract(currTile, destTile);aaaaaaaa
+                        // then continue and unit operation == true
+                     // continue back to unit operation boolean set earlier as a while
+                }
+                //Moving unit from starting location to destination
+                else
+                {
+                    m_board.MoveUnitFromSpaceToSpace(currTile, destTile);
+                }
+            }
         }
+
+        // TODO:: Refactor
+        private bool UnitsInteract(Tile currTile, Tile destTile)
+        {
+            bool unitsHaveInteracted = false;
+            byte interactionDistance = m_board.CalculateDistance(currTile, destTile);
+            if (interactionDistance == 1)
+            {
+                if (currTile.m_unit.GetJob() == Job.MAGE)
+                {
+                    byte damageDealt = 0;
+                    if (m_battleManager.calculateDamage(currTile.m_unit, destTile.m_unit, out damageDealt))
+                    {
+                        destTile.m_unit.ModifyCurrentHealth(damageDealt, false);
+                        unitsHaveInteracted = true;
+                    }
+                }
+                if (currTile.m_unit.GetJob() == Job.HEALER)
+                {
+                    byte amountHealed = 0;
+                    if (m_battleManager.calculateHealing(currTile.m_unit, destTile.m_unit, out amountHealed))
+                    {
+                        destTile.m_unit.ModifyCurrentHealth(amountHealed, true);
+                        unitsHaveInteracted = true;
+                    }
+                }
+                //test  all melee
+                if (currTile.m_unit.GetJob() == Job.FIGHTER || currTile.m_unit.GetJob() == Job.SOLDIER || currTile.m_unit.GetJob() == Job.MERCENARY)
+                {
+                    byte damageDealt = 0;
+                    if (m_battleManager.calculateDamage(currTile.m_unit, destTile.m_unit, out damageDealt))
+                    {
+                        destTile.m_unit.ModifyCurrentHealth(damageDealt, false);
+                        unitsHaveInteracted = true;
+                    }
+                }
+            }
+            //else if mages
+            else if (interactionDistance == 2)
+            {
+                if(currTile.m_unit.GetJob == Job.MAGE)
+                {
+                    byte damageDealt = 0;
+                    if(m_battleManager.calculateDamage(currTile.m_unit, destTile.m_unit, out damageDealt))
+                    {
+                        destTile.m_unit.ModifyCurrentHealth(damageDealt, false);
+                        unitsHaveInteracted = true;
+                    }
+                }
+            }
+            //esle units did not interact false
+            // if units interacted say the unit moved
+            return unitsHaveInteracted;
+        }
+
+        //selectTile must validate Tile
     }
 }
