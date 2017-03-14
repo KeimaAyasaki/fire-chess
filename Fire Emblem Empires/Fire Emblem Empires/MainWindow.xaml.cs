64e66a5de56d6554cff78b0726071ea6b0ece327
ZFlem96 <zackzacharyfleming@gmail.com>
2017-03-13 18:34:56 -0600

    load and save menu with buttons

--- Fire Emblem Empires/Fire Emblem Empires/MainWindow.xaml.cs
+++ Fire Emblem Empires/Fire Emblem Empires/MainWindow.xaml.cs
@@ -27,6 +27,7 @@ namespace Fire_Emblem_Empires
     {
         FileReader fReader = new FileReader();
         Board map;
+        private Button resume, newGame, saveGame, exitGame, saveData1, saveData2, saveData3, saveData4, loadData1, loadData2, loadData3, loadData4;
 
         public MainWindow()
         {
@@ -52,7 +53,7 @@ namespace Fire_Emblem_Empires
                 Image newScreen = new Image();
                 newScreen.Source = nextScreen;
                 this.grid.Children.Add(newScreen);
-                Button resume = new Button();
+                resume = new Button();
                 double buttonWidth = this.Width * 0.43;
                 double buttonHeight = this.Height * 0.14;
                 int heightOffset = (int)(this.Height * 0.1);
@@ -65,8 +66,9 @@ namespace Fire_Emblem_Empires
                 resume.MouseEnter += new MouseEventHandler(MouseEnters);
                 resume.MouseLeave += new MouseEventHandler(MouseExits);
                 resume.Opacity = 0;
-                //this.grid.Children.Add(resume);
-                Button newGame = new Button();
+                resume.Click += new RoutedEventHandler(ResumeGame);
+                this.grid.Children.Add(resume);
+                 newGame = new Button();
                 newGame.VerticalAlignment = VerticalAlignment.Top;
                 newGame.Margin = new Thickness(centerOffset, heightOffset += buttonDistance, 0, 0);
                 newGame.Width = buttonWidth;
@@ -76,7 +78,7 @@ namespace Fire_Emblem_Empires
                 newGame.Click += new RoutedEventHandler(NewGame);
                 newGame.Opacity = 0;
                 this.grid.Children.Add(newGame);
-                Button saveGame = new Button();
+                 saveGame = new Button();
                 saveGame.VerticalAlignment = VerticalAlignment.Top;
                 saveGame.Margin = new Thickness(centerOffset, heightOffset += buttonDistance, 0, 0);
                 saveGame.Width = buttonWidth;
@@ -84,12 +86,13 @@ namespace Fire_Emblem_Empires
                 saveGame.MouseEnter += new MouseEventHandler(MouseEnters);
                 saveGame.MouseLeave += new MouseEventHandler(MouseExits);
                 saveGame.Opacity = 0;
-                //this.grid.Children.Add(saveGame);
-                Button exitGame = new Button();
+                saveGame.Click += new RoutedEventHandler(SaveGame);
+                this.grid.Children.Add(saveGame);
+                 exitGame = new Button();
                 exitGame.VerticalAlignment = VerticalAlignment.Top;
                 exitGame.Margin = new Thickness(centerOffset, heightOffset += (int)(buttonDistance * 0.85), 0, 0);
-                exitGame.Width = buttonWidth;
-                exitGame.Height = buttonHeight;
+                exitGame.Width = Width;
+                exitGame.Height = Height;
                 exitGame.MouseEnter += new MouseEventHandler(MouseEnters);
                 exitGame.MouseLeave += new MouseEventHandler(MouseExits);
                 exitGame.Click += new RoutedEventHandler(ExitGame);
@@ -122,5 +125,120 @@ namespace Fire_Emblem_Empires
             grid.Show();
             this.Close();
         }
+        private void SaveGame(object sender, RoutedEventArgs e)
+        {
+            BitmapImage nextScreen = new BitmapImage();
+            nextScreen.BeginInit();
+            nextScreen.UriSource = new Uri(@"pack://application:,,,/" + Assembly.GetExecutingAssembly().GetName().Name + ";component/Resources/FE_Empires_SaveScreen.png");
+            nextScreen.EndInit();
+            menuScreen.Source = nextScreen;
+            this.grid.Children.Remove(resume);
+            this.grid.Children.Remove(newGame);
+            this.grid.Children.Remove(saveGame);
+            this.grid.Children.Remove(resume);
+            this.grid.Children.Remove(exitGame);
+            Image newScreen = new Image();
+            newScreen.Source = nextScreen;
+            this.grid.Children.Add(newScreen);
+            saveData1 = new Button();
+            saveData2 = new Button();
+            saveData3 = new Button();
+            saveData4 = new Button();
+            double buttonWidth = this.Width * 0.63;
+            double buttonHeight = this.Height * 0.12;
+            int heightOffset = (int)(this.Height * 0.2);
+            int centerOffset = (int)(this.Width * 0.02);
+            int buttonDistance = (int)(this.Height * 0.17);
+            saveData1.VerticalAlignment = VerticalAlignment.Top;
+            saveData1.Margin = new Thickness(-centerOffset, heightOffset, 0, 0);
+            saveData1.Width = buttonWidth;
+            saveData1.Height = buttonHeight;
+            saveData1.MouseEnter += new MouseEventHandler(MouseEnters);
+            saveData1.MouseLeave += new MouseEventHandler(MouseExits);
+            saveData1.Opacity = 0;
+            this.grid.Children.Add(saveData1);
+            saveData2.VerticalAlignment = VerticalAlignment.Top;
+            saveData2.Margin = new Thickness(-centerOffset, heightOffset += buttonDistance, 0, 0);
+            saveData2.Width = buttonWidth;
+            saveData2.Height = buttonHeight;
+            saveData2.MouseEnter += new MouseEventHandler(MouseEnters);
+            saveData2.MouseLeave += new MouseEventHandler(MouseExits);
+            saveData2.Opacity = 0;
+            this.grid.Children.Add(saveData2);
+            saveData3.VerticalAlignment = VerticalAlignment.Top;
+            saveData3.Margin = new Thickness(-centerOffset, heightOffset += buttonDistance, 0, 0);
+            saveData3.Width = buttonWidth;
+            saveData3.Height = buttonHeight;
+            saveData3.MouseEnter += new MouseEventHandler(MouseEnters);
+            saveData3.MouseLeave += new MouseEventHandler(MouseExits);
+            saveData3.Opacity = 0;
+            this.grid.Children.Add(saveData3);
+            saveData4.VerticalAlignment = VerticalAlignment.Top;
+            saveData4.Margin = new Thickness(-centerOffset, heightOffset += (int)(buttonDistance * 0.85), 0, 0);
+            saveData4.Width = buttonWidth;
+            saveData4.Height = buttonHeight;
+            saveData4.MouseEnter += new MouseEventHandler(MouseEnters);
+            saveData4.MouseLeave += new MouseEventHandler(MouseExits);
+            saveData4.Opacity = 0;
+            this.grid.Children.Add(saveData4);
+        }
+        private void ResumeGame(object sender, RoutedEventArgs e)
+        {
+            BitmapImage nextScreen = new BitmapImage();
+            nextScreen.BeginInit();
+            nextScreen.UriSource = new Uri(@"pack://application:,,,/" + Assembly.GetExecutingAssembly().GetName().Name + ";component/Resources/FE_Empires_LoadScreen.png");
+            nextScreen.EndInit();
+            menuScreen.Source = nextScreen;
+            this.grid.Children.Remove(resume);
+            this.grid.Children.Remove(newGame);
+            this.grid.Children.Remove(saveGame);
+            this.grid.Children.Remove(resume);
+            this.grid.Children.Remove(exitGame);
+            Image newScreen = new Image();
+            newScreen.Source = nextScreen;
+            this.grid.Children.Add(newScreen);
+            Button loadData1 = new Button(), loadData2 = new Button(), loadData3 = new Button(), loadData4 = new Button();
+            double buttonWidth = this.Width * 0.63;
+            double buttonHeight = this.Height * 0.12;
+            int heightOffset = (int)(this.Height * 0.2);
+            int centerOffset = (int)(this.Width * 0.02);
+            int buttonDistance = (int)(this.Height * 0.17);
+            loadData1.VerticalAlignment = VerticalAlignment.Top;
+            loadData1.Margin = new Thickness(-centerOffset, heightOffset, 0, 0);
+            loadData1.Width = buttonWidth;
+            loadData1.Height = buttonHeight;
+            loadData1.MouseEnter += new MouseEventHandler(MouseEnters);
+            loadData1.MouseLeave += new MouseEventHandler(MouseExits);
+            loadData1.Opacity = 0;
+            this.grid.Children.Add(loadData1);
+            loadData2.VerticalAlignment = VerticalAlignment.Top;
+            loadData2.Margin = new Thickness(-centerOffset, heightOffset += buttonDistance, 0, 0);
+            loadData2.Width = buttonWidth;
+            loadData2.Height = buttonHeight;
+            loadData2.MouseEnter += new MouseEventHandler(MouseEnters);
+            loadData2.MouseLeave += new MouseEventHandler(MouseExits);
+            loadData2.Opacity = 0;
+            this.grid.Children.Add(loadData2);
+            loadData3.VerticalAlignment = VerticalAlignment.Top;
+            loadData3.Margin = new Thickness(-centerOffset, heightOffset += buttonDistance, 0, 0);
+            loadData3.Width = buttonWidth;
+            loadData3.Height = buttonHeight;
+            loadData3.MouseEnter += new MouseEventHandler(MouseEnters);
+            loadData3.MouseLeave += new MouseEventHandler(MouseExits);
+            loadData3.Opacity = 0;
+            this.grid.Children.Add(loadData3);
+            loadData4.VerticalAlignment = VerticalAlignment.Top;
+            loadData4.Margin = new Thickness(-centerOffset, heightOffset += (int)(buttonDistance * 0.85), 0, 0);
+            loadData4.Width = buttonWidth;
+            loadData4.Height = buttonHeight;
+            loadData4.MouseEnter += new MouseEventHandler(MouseEnters);
+            loadData4.MouseLeave += new MouseEventHandler(MouseExits);
+            loadData4.Opacity = 0;
+            this.grid.Children.Add(loadData4);
+
+        }
     }
+
+
+    
 }
